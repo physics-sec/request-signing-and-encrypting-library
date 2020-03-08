24473cfe778655d41a99080e53638e184dc01f1e
physics-sp <physics-sp@protonmail.com>
2020-03-08 18:21:20 -0300

    add some comments and TODO

--- backend.php
+++ backend.php
@@ -10,42 +10,54 @@
 
 
 	if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/backend.php/ecdh') {
+
+		// get the POST data
 		$data = json_decode(file_get_contents('php://input'), true);
 
+		// obtain client's public key
 		$client_pub_key = $data["pubkey"];
 		error_log("client's public key: ". $client_pub_key);
 		$client_pub_key = hex2bin($client_pub_key);
 
+		// generate the server's private key
 		$secureRandom = random_bytes(32);
 		$server_private_key = curve25519_private($secureRandom);
 
+		// obtain the server's public key
 		$server_public_key  = curve25519_public($server_private_key);
 		$server_public_key  = bin2hex($server_public_key);
 		error_log("server's public key: ". $server_public_key);
 
+		// obtain shared key
 		$agreement = curve25519_shared($server_private_key, $client_pub_key);
 		$agreement = bin2hex($agreement);
+		// pass the shared key through SHA256
 		$agreement = hash("sha256", $agreement);
 		error_log("shared secret: ". $agreement);
 
+		// generate the next request id
 		$requestId = UUID::v4();
 		$response = '{"pubkey": "'. $server_public_key .'", "requestId": "'. $requestId .'"}';
 		header('Content-Type: application/json');
 
+		// save the shared key and the next request Id
 		$fp = fopen('creds.txt', 'w');
 		fwrite($fp, '{"shared" : "'. $agreement .'", "requestId": "'. $requestId .'"}');
 		fclose($fp);
 
+		// return the server's public key and the next request id
 		print $response;
 		return;
 	}
 
 	if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/backend.php') {
+		// send the index.html page
 		echo readfile("templates/index.html");
 		return;
 	}
 
 	if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/backend.php/hello') {
+		// read the shared secret and the next request id from the creds file
 		$myfile = fopen("creds.txt", "r") or die("Unable to open file!");
 		$creds  = fread($myfile, filesize("creds.txt"));
 		fclose($myfile);
@@ -54,6 +66,7 @@
 		$shared_key = $creds["shared"];
 		$requestId = $creds["requestId"];
 
+		// verify that the request is valid
 		$valid = AWS_Signature_v4::verify($shared_key, $requestId);
 
 		if (!$valid) {
@@ -61,16 +74,21 @@
 			return;
 		}
 
+		// decrypt the payload (don't forget to verify the request first!)
 		$msg = AWS_Signature_v4::getPayload($shared_key);
 		$msg = json_decode($msg, true);
 		$reflect = $msg['name'];
 
-		$shared_key = hash("sha256", $shared_key);
+		// generate a new request id
 		$requestId = UUID::v4();
+		// update the shared secret
+		$shared_key = hash("sha256", $shared_key);
+		// save the new shared secret and the next request id to the creds file
 		$fp = fopen('creds.txt', 'w');
 		fwrite($fp, '{"shared" : "'. $shared_key .'", "requestId": "'. $requestId .'"}');
 		fclose($fp);
 
+		// send the new request id back to the client
 		echo '{"msg": "hello '. $reflect .'!!", "requestId": "'. $requestId .'"}';
 		return;
 	}
--- v4.php
+++ v4.php
@@ -25,11 +25,13 @@
 			// but restore the /'s
 			$uri = str_replace( '%2F', '/', $uri );
 
-			$canonical_request[] = $uri;		// 2) URI
+			$canonical_request[] = $uri;
 
 			// 3) CanonicalQueryString
+			// TODO: format the query string in the correct way
 			$canonical_request[] = $_SERVER['QUERY_STRING'];
 
+			// get the signed headers and the signature
 			$headers = apache_request_headers();
 			$auth_header = $headers['Authorization'];
 			$signed_headers = explode("SignedHeaders=", $auth_header)[1];
@@ -61,7 +63,6 @@
 			$canonical_request[] = implode( ';', array_keys( $can_headers ) );
 
 			// 6) Payload
-			
 			$canonical_request[] = hash( "sha256", file_get_contents('php://input') );
 
 			$canonical_request = implode( "\n", $canonical_request );
@@ -79,14 +80,15 @@
 			$string = implode( "\n", $string );
 
 			// Task 3: Signature
-
 			$kSigning = hex2bin($sharedKey);
 			$signature = hash_hmac( "sha256", $string, $kSigning );
 
+			// check the validity of the signature in a constant-time manner to prevent timing attacks
 			$valid = true;
 			$length = strlen($signature);
 			for ( $i = 0; $i < $length; $i++ ) {
 				if ( $signature[$i] !== $signature_recived[$i] ) {
+					error_log("signatures do not match!");
 					$valid = false;
 				}
 			}
@@ -96,6 +98,7 @@
 			}
 
 			if ( $headers["X-Request-Id"] !== $requestId ) {
+				error_log("unexpected requestId!");
 				return false;
 			}
 
@@ -104,23 +107,36 @@
 		}
 
 		public function getPayload ( $sharedKey ) {
+			// check the header to see if the payload is encrypted
+			$headers = apache_request_headers();
+			if ($headers['X-Payload-Encrypted'] === '0') {
+				// if not, just return the payload
+				return file_get_contents('php://input');
+			}
+
+			// get the ciphertext bytes
 			$textToDecrypt = file_get_contents('php://input');
 			$encrypted = hex2bin($textToDecrypt);
 
+			// get the key bytes from the signKey
 			$key = hex2bin($sharedKey);
 
-			$headers = apache_request_headers();
+			// get the initialization vector bytes from the header
 			$iv = $headers['X-IV'];
 			$iv = hex2bin($iv);
 
+			// get the authentication tag
 			// the WebCryptoAPI adds the tag (of 128 bits by default) at the end of the ciphertext
 			$tag_length = 128;
 			$tag = substr($encrypted, -($tag_length/8));
 
+			// get ciphertext without tag
 			$ciphertext = substr($encrypted,0, strlen($encrypted) - ($tag_length/8));
 
+			// decrypt the ciphertext
 			$decrypted = openssl_decrypt($ciphertext, 'aes-256-gcm', $key, OPENSSL_RAW_DATA, $iv, $tag);
 
+			// return the platintext
 			return $decrypted;
 		}
 	}
